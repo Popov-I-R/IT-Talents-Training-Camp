c4b4eab86044cc84bac8b3d223d86cfab0f5386a
Popov-I-R <farm03007@gmail.com>
2022-06-21 21:52:20 +0300

    Create 4. Task four.js

---
+++ Lesson Two/1. Homework/4. Task four.js
@@ -0,0 +1,31 @@
+var firstVar = 27;
+var secondVar = 25;
+
+if (firstVar > secondVar) {
+    console.log(secondVar, firstVar);
+} else {
+    console.log(firstVar, secondVar)
+}
+
+
+
+
+
+
+
+//-----------OLD SOLUTION--numbers-and-strings------------
+// var firstVar = 'ccc';
+// var secondVar = 'bbb';
+// if(isNaN(firstVar && secondVar)){ // If its NaN
+//     if (secondVar.localeCompare(firstVar) === 1) {
+//         console.log(firstVar, secondVar)
+//     } else {
+//         console.log(secondVar, firstVar)
+//     }
+//  } else { // If its number
+//     if (firstVar > secondVar) {
+//         console.log(secondVar, firstVar)
+//     } else {
+//         console.log(firstVar, secondVar)
+//     }
+//  }
