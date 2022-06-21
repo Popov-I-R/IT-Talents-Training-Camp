647ad685cddc01ca02491742241898ef686dca6e
Popov-I-R <farm03007@gmail.com>
2022-06-21 21:52:13 +0300

    Create 3. Task three - three solutions.js

---
+++ Lesson Two/1. Homework/3. Task three - three solutions.js
@@ -0,0 +1,32 @@
+// Решение 1 - Математика
+
+var first = 3;
+var second = 2;
+
+first = first + second; // 3 + 2 = 5
+second = first - second; // 5 - 2 = 3
+first = first - second; // 5 - 3 = 2
+
+console.log(first, second);
+
+// Решение 2 - Временна променлива----------------
+
+// var first = 3;
+// var second = 2;
+
+// var temporary = first;
+
+// first = second;
+// second = temporary;
+
+// console.log(first, second);
+//------------------------------------------------
+
+// // Решение 3 - Деструктуриране
+
+// var first = "abv";
+// var second = 123;
+
+// [first, second] = [second, first]
+
+// console.log(first, second);
