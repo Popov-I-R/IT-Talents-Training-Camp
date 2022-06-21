a729bec66ce6a8354d982f936be71993b5052272
Popov-I-R <farm03007@gmail.com>
2022-06-21 21:52:08 +0300

    Create 2. Task two - two solutions.js

---
+++ Lesson Two/1. Homework/2. Task two - two solutions.js
@@ -0,0 +1,40 @@
+console.log(`Решение с целочислени числа \n`)// Решение с целочислени числа
+
+var firstNum = 6;
+var secondNum = 2;
+
+var sum = firstNum + secondNum;
+var substract = firstNum - secondNum;
+var multiply = firstNum * secondNum;
+var moduleDivide = firstNum % secondNum;
+var divide = firstNum / secondNum;
+
+console.log(`sum: ${sum}`);
+console.log(`substract: ${substract}`);
+console.log(`multiply: ${multiply}`);
+console.log(`moduleDivide: ${moduleDivide}`);
+console.log(`divide: ${divide}`);
+
+//-----------------------------------------------------
+console.log(`\n Решение / числа с плаваща запетая \n`)// Решение / цисла с плаваща запетая
+
+var firstNum = 5.2;
+var secondNum = 4.3;
+
+var sum = firstNum + secondNum;
+var substract = firstNum - secondNum;
+var multiply = firstNum * secondNum;
+var moduleDivide = firstNum % secondNum;
+var divide = firstNum / secondNum;
+
+// console.log(`sum: ${sum.toFixed(2)}`);
+// console.log(`substract: ${substract.toFixed(2)}`);
+// console.log(`multiply: ${multiply.toFixed(2)}`);
+// console.log(`moduleDivide: ${moduleDivide.toFixed(2)}`);
+// console.log(`divide: ${divide.toFixed(2)}`);
+
+console.log(`sum: ${sum}`);
+console.log(`substract: ${substract}`);
+console.log(`multiply: ${multiply}`);
+console.log(`moduleDivide: ${moduleDivide}`);
+console.log(`divide: ${divide}`);
