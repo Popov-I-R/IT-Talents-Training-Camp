e0a561d273d57b106e1787fb4c6bd9708c2eac9a
Popov-I-R <farm03007@gmail.com>
2022-06-22 23:04:00 +0300

    Create 5. task 5.js

---
+++ Lesson Two/2. Optional tasks/5. task 5.js
@@ -0,0 +1,13 @@
+var language = "缩写";
+
+if (language === "en") {
+  console.log(`Hello World`);
+} else if (language === "es") {
+  console.log(`Hola Mundo`);
+} else if (language === "de") {
+  console.log(`Hallo Welt`);
+} else if (language === "bg") {
+  console.log(`Здравей свят`);
+} else if (language === "缩写") {
+  console.log(`你好世界`);
+}
