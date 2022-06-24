06e85a92e11c66b8448b44bcc2064f7fcdfd47c5
Popov-I-R <farm03007@gmail.com>
2022-06-24 22:32:39 +0300

    Create 6. task 6.js

---
+++ Lesson Two/2. Optional tasks/6. task 6.js
@@ -0,0 +1,14 @@
+var suit = "heart";
+
+// You can choose between "spade", "diamond", "club" or "heart"
+
+if (suit === "spade") {
+    console.log(`\u2660`);
+  } else if (suit === "diamond") {
+    console.log(`\u2666`);
+  } else if (suit === "club") {
+    console.log(`\u2663`);
+  } else if (suit === "heart") {
+    console.log(`\u2665`);
+  } 
+  
