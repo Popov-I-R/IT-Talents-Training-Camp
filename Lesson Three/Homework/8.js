9551f7f88b2123ef9083344f12e9e70ff9a502d6
Popov-I-R <farm03007@gmail.com>
2022-06-26 00:48:08 +0300

    Create 8.js

---
+++ Lesson Three/Homework/8.js
@@ -0,0 +1,70 @@
+var n = 2;
+
+var result = "";
+var num = "";
+
+result = n - 1;
+
+ for (var i = 1; i <= n; i++) { 
+  num = result; 
+    for (var j = 1; j <= n -1; j++) {
+        num += result + ""
+    }
+
+    result +=2;
+    console.log(num);
+ }
+
+   
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+// var n = 3;
+// var result  = '';
+
+// for(var i = 1; i <=n; i++) {
+
+//     result =  result + n;
+    
+    
+//   }
+//   console.log(result)
+
+
+
+
+/*
+var n = 4;
+var n2 = n-1
+var n3 = n2 + 2
+var result  = '';
+var result2 = '';
+var result3 = '';
+
+
+
+for(var i = 1; i <=n; i++) {
+  result =  result + n;
+  result2 = result2 + n2
+  result3 = result3 + n3
+}
+console.log(result)
+console.log(result2)
+console.log(result3)
+
+ до някъде върши работа за 7ма задача, обаче console.log не е динамичен
+*/
+
