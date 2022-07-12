f374c592d83776bba953805c251bef6154a3d1f5
Popov-I-R <farm03007@gmail.com>
2022-07-12 11:25:56 +0300

    Create 4h.js

---
+++ Lesson Three/Optional tasks/2. Figures tasks/4h.js
@@ -0,0 +1,46 @@
+var n = 7;
+var result = "";
+
+for (var outside = 1; outside <= n; outside++) {
+  if (outside === 1 || outside === n) {
+    // Горе и долу
+    for (var upAndDown = 1; upAndDown <= n; upAndDown++) {
+      result += "#";
+    }
+  }
+
+  if (outside === 2 || outside === n - 1) {
+    // втори и пред последен ред
+    for (var secondAndPre = 1; secondAndPre <= n; secondAndPre++) {
+      if (secondAndPre === 2 || secondAndPre === n - 1) {
+        result += "#";
+      } else {
+        result += " ";
+      }
+    }
+  }
+
+  if (outside === 3 || outside === n - 2) {
+    for (var thirdAndFifth = 1; thirdAndFifth <= n; thirdAndFifth++) {
+      if (thirdAndFifth === 3 || thirdAndFifth === n - 2) {
+        result += "#";
+      } else {
+        result += " ";
+      }
+    }
+  }
+
+  if (outside === 4) {
+    // Среда
+    for (var middle = 1; middle <= n; middle++) {
+      if (middle === 4) {
+        result += "#";
+      } else {
+        result += " ";
+      }
+    }
+  }
+
+  console.log(result);
+  result = "";
+}
